1ec745ce64563566cfa274e6c2a76734286ee9de
Brad Copenhaver and daniel munger <brad@>
2016-10-25 10:09:49 -0700

    create a vowel-replacement function.

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+var original;
+var byLetter = "";
+var vowels = ["A", "a", "E", "e", "I", "i", "O", "o", "U", "u"]
+
+var letters = [];
+var newSentence = "";
+
+var splitString = function(stringToSplit, separator) {
+  return words = stringToSplit.split(separator);
+}
+
+var replaceVowels = function(arrayOfLetters) {
+  for (index=0; index<arrayOfLetters.length; index+=1){
+    for (i=0; i<vowels.length; i+=1){
+      if (arrayOfLetters[index] === vowels[i]){
+        arrayOfLetters[index] = "-";
+      }
+
+    }
+  }
+}
+
+var toSentence = function(arrayOfLetters) {
+  for(i=0; i<arrayOfLetters.length; i+=1){
+    newSentence += arrayOfLetters[i];
+  }
+
+};
+
+//
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+
+    original = $("input#original").val();
+
+    letters = splitString(original, byLetter);
+
+    replaceVowels(letters);
+    toSentence(letters);
+    $(".output").text(newSentence);
+    $("form").hide();
+    });
+});
